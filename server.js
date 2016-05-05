fc2a3ff9e5f6ae6bfe4fbe3ca1fc9334712c1846
aidarbuy <aidarbuy@gmail.com>
2016-05-05 22:08:48 +0600

    Add https

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+var fs = require('fs');
+var options = {
+	key:  fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem'),
+};
 var express = require('express');
 var app = express();
+var server = require('https').createServer(options, app);
 
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/dashboard.html');
 });
 
 var PORT = process.env.PORT || 4200;
-app.listen(PORT, function() {
+server.listen(PORT, function() {
 	console.log('Listening on port', PORT);
 });
