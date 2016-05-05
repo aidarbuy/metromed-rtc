7c515e5eb377e10c0fb048c0ecb6ccab7a292f4a
aidarbuy <aidarbuy@gmail.com>
2016-05-05 20:05:58 +0600

    Remove views and public, reinstall node modules

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ app.use(function (req, res, next) {
   // res.setHeader('Access-Control-Allow-Origin', '95.56.141.234');
   // res.setHeader('Access-Control-Allow-Origin', 'https://www.metromeduc.com');
   res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
-
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
   // Pass to next layer of middleware
   next();
 });
