10fa7ec55fb3e4bc075cda968e9ce27424de26ba
aidarbuy <aidarbuy@gmail.com>
2016-05-05 22:30:54 +0600

    Add https_server.js

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node https_server.js
---
+++ https_server.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var express = require('express');
+var https = require('https');
+var key = fs.readFileSync('./key.pem');
+var cert = fs.readFileSync('./cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
+var PORT = 4200;
+var HOST = 'localhost';
+app = express();
+app.use(express.static(__dirname + '/public'));
+
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+
+
+// routes
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/dashboard.html');
+});
+app.post('/ho', function(req, res) {
+    res.send('HO!');
+});
