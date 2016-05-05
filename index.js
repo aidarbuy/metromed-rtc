2796448b14aa3dbc8d34f2bf93e800cd995d5f41
aidarbuy <aidarbuy@gmail.com>
2016-05-05 20:27:17 +0600

    Mute headers

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ var app = require('express')();
 var origins = 'https://localhost:3000';
 
 // Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', '95.56.141.234');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://www.metromeduc.com');
-  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', '95.56.141.234');
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://www.metromeduc.com');
+//   res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // var server = require('https').createServer(options, app);
 var server = require('https').createServer(app);
